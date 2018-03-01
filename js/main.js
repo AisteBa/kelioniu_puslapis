3c8d5c25314ebe575189125c1d7e8697299b6273
Irmantas <irmantas.cernauskas@gmail.com>
2018-03-01 21:03:48 +0200

    pakeitimai

--- js/main.js
+++ js/main.js
@@ -6,9 +6,23 @@ $(document).ready(function(){
       },800);
   });
 
+  $("#travelButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#listContainer").offset().top
+    },800);
+  });
+
+  $("#travelButton1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#googleMapsContainer").offset().top
+    },800);
+  });
+
   $('.parallax').parallax();
   Materialize.updateTextFields();
   
     $('.carousel').carousel();
     $('.carousel').carousel('prev');
-  });
+
+});
+
